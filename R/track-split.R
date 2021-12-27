e8d503986b336ff5069a449c466dc8d05380f881
YBK <ybkamaleri@users.noreply.github.com>
2021-12-27 18:34:05 +0100

    Doc: Edit text

--- R/track-split.R
+++ R/track-split.R
@@ -1,7 +1,8 @@
 #' Get geo code that are split after code change
 #'
 #' @inheritParams get_code
-#' @return Dataset of class `data.table` with column 'split' showing how many the codes have been split to
+#' @return Dataset of class `data.table` with column `split` showing the number
+#'   of time the codes have been split to
 #' @examples
 #'  dt <- track_split("kommune", 2018, 2020)
 #' @export
