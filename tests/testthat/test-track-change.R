3411e5d35445de8ddc731380af9d1b80afaec92d
Yusman Kamaleri <ybkamaleri@gmail.com>
2022-02-02 15:38:04 +0100

    Test: Merge and split codes

--- tests/testthat/test-track-change.R
+++ tests/testthat/test-track-change.R
@@ -5,3 +5,13 @@ test_that("Track change", {
   expect_equal(track_change("fylke", 2018, 2020), dtOut)
   expect_equal(track_change("kommune", 2017, 2020), dtKomm)
 })
+
+test_that("Track split", {
+  dtOut <- readRDS(system.file("test-data", "split_2018_2020.rds", package = "norgeo"))
+  expect_equal(track_split("k", 2018, 2020), dtOut)
+})
+
+test_that("Track merge", {
+  dtOut <- readRDS(system.file("test-data", "merge_2018_2020.rds", package = "norgeo"))
+  expect_equal(track_merge("k", 2018, 2020), dtOut)
+})
