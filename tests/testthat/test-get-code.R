3b230c22966622c59b7760051f2e4a574193de1d
Yusman Kamaleri <ybkamaleri@gmail.com>
2022-02-02 14:36:40 +0100

    Test: Code At

--- tests/testthat/test-get-code.R
+++ tests/testthat/test-get-code.R
@@ -1,3 +1,8 @@
+test_that("get code at", {
+  dtOut <- readRDS(system.file("test-data", "codeAt.rds", package = "norgeo"))
+  expect_equal(get_code(type = "fylke", 2018), dtOut)
+})
+
 test_that("get codes - all", {
   vcr::use_cassette("codes-fromto", {
     dt <- get_code(type = "fylke", from = 2018, to = 2020)
