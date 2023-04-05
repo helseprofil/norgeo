33991af643a11fb932358492e490eacd038ce285
Yusman Bin Kamaleri <ybkamaleri@gmail.com>
2023-04-05 16:32:10 +0200

    Test: Update test data due to changes in the server

--- tests/testthat/test-cast-geo.R
+++ tests/testthat/test-cast-geo.R
@@ -1,6 +1,5 @@
 test_that("Cast geo", {
-  dt <- readRDS(system.file("test-data", "cast_2020.rds", package = "norgeo"))
+  dt <- readRDS(system.file("test-data", "cast_2010.rds", package = "norgeo"))
 
-  expect_equal(cast_geo(2020), dt)
-
+  expect_equal(cast_geo(2010), dt)
 })
--- tests/testthat/test-track-change.R
+++ tests/testthat/test-track-change.R
@@ -1,9 +1,9 @@
 test_that("Track change", {
-  dtOut <- readRDS(system.file("test-data", "trackChangeFylke_2018_2020.rds", package = "norgeo"))
-  dtKomm <- readRDS(system.file("test-data", "trackChangeKomm_2017_2020.rds", package = "norgeo"))
+  dtOut <- readRDS(system.file("test-data", "trackChangeFylke_2017_2018.rds", package = "norgeo"))
+  dtKomm <- readRDS(system.file("test-data", "trackChangeKomm_2010_2015.rds", package = "norgeo"))
 
-  expect_equal(track_change("fylke", 2018, 2020), dtOut)
-  expect_equal(track_change("kommune", 2017, 2020), dtKomm)
+  expect_equal(track_change("fylke", 2017, 2018), dtOut)
+  expect_equal(track_change("kommune", 2010, 2015), dtKomm)
 })
 
 test_that("Track split", {
